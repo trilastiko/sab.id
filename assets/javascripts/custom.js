23a927bdb937422abe139ce546bf5fe16f488409
andi46 <andi46@users.noreply.github.com>
2019-04-15 15:40:34 +0700

    Update counter and footer
    Update counter and footer

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+
+/* 
+=============================================== 
+Work slide
+=============================================== 
+*/
+var swiper = new Swiper('.swiper-container', {
+  	  slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+	
     /* 
     =============================================== 
     JS Code before Minified
@@ -12,7 +32,7 @@ $(document).ready(function() {
         mouseDrag: false,
     });
 
-    $('.works-sab-slide').owlCarousel({
+    $('.works-sab-slide-x').owlCarousel({
         center: false,
         items: 1,
         loop: false,
@@ -76,14 +96,14 @@ $(document).ready(function() {
         var myLatlng = new google.maps.LatLng(-6.238845, 106.812077);
         var imagePath = 'https://www.sabtech.id/wp-content/uploads/2019/04/google-location-icon-sab.png'
         var mapOptions = {
-            zoom: 19,
+            zoom: 15,
             center: myLatlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
 
         var map = new google.maps.Map(document.getElementById('map'), mapOptions);
         //Callout Content
-        var contentString = 'sabtech';
+        var contentString = 'PT Sinergi Antar Benua';
         //Set window width + content
         var infowindow = new google.maps.InfoWindow({
             content: contentString,
@@ -120,101 +140,10 @@ JS Counter
 =============================================== 
 */
 
-(function($) {
-    $.fn.countTo = function(options) {
-        options = options || {};
-
-        return $(this).each(function() {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-                from: $(this).data('from'),
-                to: $(this).data('to'),
-                speed: $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals: $(this).data('decimals')
-            }, options);
-
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
-
-            // references & variables that will change with each update
-            var self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
-
-            $self.data('countTo', data);
-
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
-            }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-            // initialize the element with the starting value
-            render(value);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-
-                render(value);
-
-                if (typeof(settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-
-                    if (typeof(settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-
-            function render(value) {
-                var formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue);
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0, // the number the element should start at
-        to: 0, // the number the element should end at
-        speed: 1000, // how long it should take to count between the target numbers
-        refreshInterval: 100, // how often the element should be updated
-        decimals: 0, // the number of decimal places to show
-        formatter: formatter, // handler for formatting the value before rendering
-        onUpdate: null, // callback method for every time the element is updated
-        onComplete: null // callback method for when the element finishes updating
-    };
-
-    function formatter(value, settings) {
-        return value.toFixed(settings.decimals);
-    }
-}(jQuery));
-
-jQuery(function($) {
-    // custom formatting example
-    $('.count-number').data('countToOptions', {
-        formatter: function(value, options) {
-            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-        }
+jQuery(document).ready(function( $ ) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
     });
-
-    // start all the timers
-    $('.timer').each(count);
-
-    function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
-    }
 });
+
