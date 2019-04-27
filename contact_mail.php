31c345a71c8e1be6364718da5f930506a2fbcea0
trilastiko <trilastiko@gmail.com>
2019-04-27 16:19:11 +0700

    Update contact_mail.php
    change admin@sab.id to mail@sab.id

--- contact_mail.php
+++ contact_mail.php
@@ -1,5 +1,5 @@
 <?php
-$toEmail = "admin@sab.id";
+$toEmail = "mail@sab.id";
 $subject = "Contact form from website sab.id";
 $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
 if(mail($toEmail, $subject, $_POST["message"], $mailHeaders)) {
