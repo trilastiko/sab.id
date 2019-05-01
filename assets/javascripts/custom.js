825d8b93192ee388c8da464f71f99377ff0912c4
trilastiko <trilastiko@gmail.com>
2019-05-01 19:15:10 +0700

    Update custom.js

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -4,7 +4,7 @@
 
     var SAB = {
 
-            //----------- 1. To top Jquery ----------- 
+            //----------- 1. To top Jquery -----------
             theme_scrollUP: function() {
                 var toTop = $('.to-top');
                  toTop.on('click', function(e) {
@@ -14,7 +14,7 @@
             },
 
 
-            //----------- 2. Counter ----------- 
+            //----------- 2. Counter -----------
             theme_counter: function() {
                 if ( $( ".count-sab" ).length ) {
                 $(window).scroll(testScroll);
@@ -96,10 +96,10 @@
                          e.preventDefault(); // prevent hard jump, the default behavior
                         $(".team-list li").removeClass("active current");
                         $(this).addClass("active");
-                        
+
                         var headerHeight = $("header.nav-sab").outerHeight();
-                        var target = $('#info-people-id');   
-                        $('html, body').animate({ scrollTop: target.offset().top - (headerHeight + 15) }, 500);  
+                        var target = $('#info-people-id');
+                        $('html, body').animate({ scrollTop: target.offset().top - (headerHeight + 15) }, 500);
 
                         var name = $(this).attr('data-name'),
                             img = $(this).attr('data-image'),
@@ -109,7 +109,7 @@
                             tw = $(this).attr('data-twitter'),
                             ig = $(this).attr('data-instagram'),
                             ln = $(this).attr('data-linkedin');
-                        
+
                             $('.info-name').text(name);
                             $('.info-position').text(position);
                             $('.info-description').text(desc);
@@ -123,10 +123,10 @@
                 });
             },
 
-            //---------- 5. Isotope Metafizy ----------- 
+            //---------- 5. Isotope Metafizy -----------
             theme_works_isotope: function(){
                 if ($.fn.isotope) {
-                
+
                 // init Isotope
                 var $grid = $('.grid').isotope({
                     itemSelector: '.items'
@@ -142,7 +142,7 @@
                     path: '.pagination__next' ,
                     // status: '.page-load-status',
                     button: '.view-more-button',
-                    // using button, disable loading on scroll 
+                    // using button, disable loading on scroll
                     scrollThreshold: false,
                     hideNav: '.pagination__next'
                 });
@@ -173,7 +173,7 @@
                         path: '.pagination__next' ,
                         // status: '.page-load-status',
                         button: '.view-more-button',
-                        // using button, disable loading on scroll 
+                        // using button, disable loading on scroll
                         scrollThreshold: false,
                         hideNav: '.pagination__next'
                     });
@@ -186,15 +186,15 @@
                 });
 
                 function updateFilterCount() {
-                    $filterCount.text( 'We Found ' + iso.filteredItems.length + ' example design' );
+                    $filterCount.text( 'Here are ' + iso.filteredItems.length + ' of SAB Portfolios' );
                 }
-                  
+
                   updateFilterCount();
 
                   $grid.on( 'append.infiniteScroll', function() {
                     updateFilterCount();
                   });
-                  
+
                   // change is-checked class on buttons
                   $('.portfolio-sab--filter a').each( function( i, buttonGroup ) {
                     var $buttonGroup = $( buttonGroup );
@@ -213,19 +213,19 @@
                     loop: true,
                     slidesPerView: 2,
                     spaceBetween: 40,
-                
+
                     // If we need pagination
                     pagination: {
                       el: '.swiper-pagination',
                       type: 'custom'
                     },
-                
+
                     // Navigation arrows
                     navigation: {
                       nextEl: '.swiper-button-next',
                       prevEl: '.swiper-button-prev',
                     },
-                
+
                     // And if we need scrollbar
                     /*scrollbar: {
                       el: '.swiper-scrollbar',
@@ -243,20 +243,20 @@
                           slidesPerView: 1,
                           spaceBetween: 20
                         },
-          
+
                     }
                   })
                 }
-               
+
             },
 
-            //---------- 6. Testimonial Section ----------- 
+            //---------- 6. Testimonial Section -----------
             theme_testi: function() {
               if ( $( ".testimony-sab-slide" ).length ) {
                 var swiper = new Swiper('.testimony-sab-slide', {
                       direction: 'vertical',
                       effect: 'fade',
-                      autoHeight: true, 
+                      autoHeight: true,
                        autoplay: {delay: 3000},
                         loop: true,
                         pagination: {
@@ -276,10 +276,10 @@
                           el: '.swiper-scrollbar',
                           draggable: false,
                         },
-                      
+
                   });
               }
-            }, 
+            },
 
             //---------- 7. Work Section -----------
             theme_works: function() {
@@ -293,7 +293,7 @@
                     $this.find(".swiper-button-next").addClass("btn-next-" + index);
                     $this.find(".swiper-pagination").addClass("swiper-pagination-" + index);
                     var swiper = new Swiper(".instance-" + index, {
-                        
+
                         slidesPerView: 'auto',
                         spaceBetween: 10,
                         loop: true,
@@ -312,7 +312,7 @@
                         nextEl: '.btn-next-' + index,
                         //prevEl: '.works-sab-pagination',
                         },
-          
+
 
                     });
                 });
@@ -358,15 +358,15 @@
                     var sections = $('.works-sab-slide');
                     var currentScroll = $(this).scrollTop();
                     var currentSection;
-                    sections.each(function(i){      
+                    sections.each(function(i){
                       var divPosition = $(this).offset().top;
                       if( divPosition - 200 < currentScroll ){
                         currentSection = $(this);
                          $('.nav-works-sab a.active').removeClass('active');
                          $('.nav-works-sab a').eq(i).addClass('active');
                       }
-                      
-                      
+
+
                     })
                     }
 
@@ -428,8 +428,8 @@
 
                 });
                 }
-            },    
- 
+            },
+
 
             // theme init
             theme_init: function() {
