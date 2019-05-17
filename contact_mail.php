f9a6e32929532f4584c82a9ac2af347361811fb0
Abdul <alik.dagdigdug@gmail.com>
2019-05-17 22:47:59 +0700

    update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,10 +1,18 @@
 <?php
-$toEmail = "mail@sab.id";
-$subject = "Contact form from website sab.id";
-$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
-if(mail($toEmail, $subject, $_POST["message"], $mailHeaders)) {
-print "<p class='success'>Contact Mail Sent.</p>";
-} else {
-print "<p class='Error'>Problem in Sending Mail.</p>";
-}
+include "classes/class.phpmailer.php";
+$mail = new PHPMailer; 
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl'; 
+$mail->Host = "banshee.mxlogin.com"; //host masing2 provider email
+$mail->SMTPDebug = 2;
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->Username = "mail@sab.id"; //user email
+$mail->Password = "APn7Tyue3t,N"; //password email 
+$mail->SetFrom($_POST["userEmail"],$_POST["userName"]); //set email pengirim
+$mail->Subject = "Contact form from website sab.id"; //subyek email
+$mail->AddAddress("mail@sab.id","SAB");  //tujuan email
+$mail->MsgHTML($_POST["message"]);
+if($mail->Send()) echo "<p class='success'>Contact Mail Sent.</p>";
+else echo "<p class='Error'>Problem in Sending Mail.</p>";
 ?>
