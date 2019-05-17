bc22666e7266d4f65597b6fb3c9644a1bd78d200
Abdul <alik.dagdigdug@gmail.com>
2019-05-17 22:37:59 +0700

    update mail.php

--- mail.php
+++ mail.php
@@ -3,15 +3,15 @@ include "classes/class.phpmailer.php";
 $mail = new PHPMailer; 
 $mail->IsSMTP();
 $mail->SMTPSecure = 'ssl'; 
-$mail->Host = "smtp.gmail.com"; //host masing2 provider email
+$mail->Host = "banshee.mxlogin.com"; //host masing2 provider email
 $mail->SMTPDebug = 2;
 $mail->Port = 465;
 $mail->SMTPAuth = true;
-$mail->Username = "alik.dagdigdug@gmail.com"; //user email
-$mail->Password = "Lengkong88"; //password email 
-$mail->SetFrom("mail@domain.com","Nama pengirim"); //set email pengirim
+$mail->Username = "mail@sab.id"; //user email
+$mail->Password = "APn7Tyue3t,N"; //password email 
+$mail->SetFrom("alik.dagdigdug@gmail.com","Abdul"); //set email pengirim
 $mail->Subject = "Testing"; //subyek email
-$mail->AddAddress("alik.dagdigdug@gmail.com","Abdul");  //tujuan email
+$mail->AddAddress("mail@sab.id","SAB");  //tujuan email
 $mail->MsgHTML("Testing...");
 if($mail->Send()) echo "Message has been sent";
 else echo "Failed to sending message";
